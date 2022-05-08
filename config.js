1a823ad50e2140b238ed787f869c4c6bc1d30904
huydz07 <105117126+huydz07@users.noreply.github.com>
2022-05-08 16:15:47 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const CONFIG = {
-    titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
+    titleWeb: "Gửi em bé của anh",
+    introTitle: 'Mai Thị Kim Ngọc',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
